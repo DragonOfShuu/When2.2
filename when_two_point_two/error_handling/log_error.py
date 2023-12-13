17ffa48160ed9b14f30d436835399c5e9ce02da5
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2023-12-13 00:09:34 -0700

    Error Folder is Made Now

--- when_two_point_two/error_handling/log_error.py
+++ when_two_point_two/error_handling/log_error.py
@@ -1,11 +1,15 @@
+from pathlib import Path
 import traceback
 from datetime import datetime
 
-error_log_location = "error_logs"
+error_log_location = Path("error_logs")
 
 def log_it(e: Exception):
     now = datetime.now()
 
-    with open(f'{error_log_location}/when_2.2_error_{now.strftime("%d-%m-%Y_%H-%M-%S")}.log', 'w') as f:
+    if not error_log_location.exists():
+        error_log_location.mkdir()
+
+    with open(error_log_location/ f'when_2.2_error_{now.strftime("%d-%m-%Y_%H-%M-%S")}.log', 'w') as f:
         f.write(f"Error: {e.args[0]}\n\n"+traceback.format_exc())
         
